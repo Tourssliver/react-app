28ceb0abd706c096d8f31a266f828c61a72a4093
Tourssliver <1341198325@qq.com>
2020-01-26 20:56:40 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link}from "react-router-dom";
+import { Layout } from 'antd';
 
+
+
+const { Header, Footer, Sider, Content } = Layout;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+        <div>
+        <Layout>
+          <Header>
+            <Link to="/login">登录</Link>
+            <Link to="/admin">主页</Link>
+          </Header>
+          <Content>
+          <Switch>
+            {/* <Route exact path="/" component={Login} />
+            <Route exact path="/login" component={Login} />
+            <Route path="/admin" component={Admin} /> */}
+          </Switch>
+          </Content>
+        </Layout>
+        </div>
+      </Router>
     </div>
   );
 }
